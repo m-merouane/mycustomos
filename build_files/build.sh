08e8b13807533fe50a500e17aa54c4536823941e
m-merouane <merouane@qualix.lu>
2025-08-13 12:51:25 +0200

    add more sw

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux krita discord
+dnf5 install -y tmux
+flatpak install app/com.discordapp.Discord/x86_64/stable
 
 # Use a COPR Example:
 #
